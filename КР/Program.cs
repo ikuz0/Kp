3c0d384d7174155c672b212ddbe576b7a26d1f05
IkuzO <Kuznetsovilya469@yandex.ru>
2022-09-19 13:49:06 +0300

    вывод массива на экран

--- КР/Program.cs
+++ КР/Program.cs
@@ -1,2 +1,33 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿void FillArray(string[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.WriteLine($"Введите {i + 1}-й элемент: >> ");
+        array[i] = Console.ReadLine()!;
+
+    }
+}
+
+void PrintArray(string[] array)
+{
+    int count = array.Length;
+
+
+    Console.WriteLine($"Входной массив: ");
+    Console.Write($">> ");
+    for (int i = 0; i < count; i++)
+    {
+        Console.Write($"{array[i]} ");
+    }
+    Console.WriteLine();
+}
+
+
+
+Console.WriteLine("Введите колличество элементов: ");
+int num = int.Parse(Console.ReadLine()!);
+string[] array = new string[num];
+FillArray(array);
+PrintArray(array);
+
+
